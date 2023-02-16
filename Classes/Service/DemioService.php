c277817306365149828c746e7c85bab1f31d17e5
André Lademann <andre.lademann@netresearch.de>
2023-02-16 17:14:02 +0100

    Add doc blocks

--- Classes/Service/DemioService.php
+++ Classes/Service/DemioService.php
@@ -7,24 +7,30 @@ use GuzzleHttp\Client;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 
-
-
+/**
+ * Class DemioService
+ * @package Netresearch\T3Demio\Service
+ */
 class DemioService implements LoggerAwareInterface
 {
 
+    /**
+     * Use the logger trait to get a logger in your class
+     */
     use LoggerAwareTrait;
+
     /**
      * API URL from demio
      */
     const API_URL = 'https://my.demio.com/api/v1/events?type=past';
 
     /**
-     * @var Client
+     * @var Client $httpClient The HTTP client
      */
     protected $httpClient;
 
     /**
-     * @var array
+     * @var array $settings The extension settings
      */
     protected $settings;
 
@@ -36,6 +42,13 @@ class DemioService implements LoggerAwareInterface
         $this->settings = $extensionConfiguration->get('t3_demio');
     }
 
+    /**
+     * Fetches events from the demio API
+     *
+     * @param string $type
+     * @throws \RuntimeException
+     * @return mixed
+     */
     public function fetchEventsFromApi(string $type = 'all'):mixed
     {
         $headers = [
@@ -48,12 +61,14 @@ class DemioService implements LoggerAwareInterface
             'headers' => $headers
         ]);
         $statusCode = $response->getStatusCode();
+        
         if ($statusCode === 200) {
             $responseData = json_decode($response->getBody(), true);
             return $responseData;
         } else {
-            $this->logger->error('API request failed with status code ' . $statusCode);
-            throw new \RuntimeException('API request failed with status code ' . $statusCode);
+            $error = 'API request failed with status code ' . $statusCode;
+            $this->logger->error($error);
+            throw new \RuntimeException($error);
         }
     }
 }
